44acc220ec91e4e19e4b5f809e517fc884577096
Anderson0017 <andeerson0017@gmail.com>
2020-06-14 20:42:54 -0300

    quantity followers

--- components/Cards.js
+++ components/Cards.js
@@ -4,33 +4,49 @@ const Cards = () => (
     <div className="content">
       <ul>
         <li className="facebook">
-        
           <header>
             <img src="/icon-facebook.svg" alt="icon-facebook"/>
             <strong>Anderson silva</strong>
           </header>
+          <section className="followers">
+            <h1>11220</h1>
+            <span>FOLLOWERS</span>
+          </section>
         </li>
+
         <li className="linkedin">
           <header>
             <img src="/icon-twitter.svg" alt="linkedin"/>
             <strong>Anderson Silva</strong>
           </header>
-
+          <section className="followers">
+            <h1>11220</h1>
+            <span>FOLLOWERS</span>
+          </section>
         </li>
+
         <li className="instagram">
           <header>
             <img src="/icon-instagram.svg" alt="instagram"/>
             <strong>Anderson silva</strong>
           </header>
-
+          <section className="followers">
+            <h1>11220</h1>
+            <span>FOLLOWERS</span>
+          </section>
         </li>
+
         <li className="github">
           <header>
             <img src="/icon-youtube.svg" alt="github"/>
             <strong>Anderson silva</strong>
           </header>
-
+          <section className="followers">
+            <h1>11220</h1>
+            <span>FOLLOWERS</span>
+          </section>
         </li>
+
       </ul>
     </div>
 
@@ -72,6 +88,25 @@ const Cards = () => (
         margin-left: 10px;
         color: hsl(228, 34%, 66%);
         font-size: 14px;
+      }
+
+      .content li section.followers {
+        display:flex;
+        flex-direction: column;
+        align-items: center;
+
+        margin-top: 20px;
+      }
+
+      .content li section.followers h1 {
+        font-size: 22px;
+        color: #FFF;
+      }
+
+      .content li section.followers span {
+        color: hsl(228, 34%, 66%);
+        font-size: 18px;
+        margin-top: 5px;
       }
 
       .content li.facebook {
